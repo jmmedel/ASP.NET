85d0549ed6ea7d0fa0bc25e58870642ca87c1bcf
john <jm.kagaya@gmail.com>
2018-02-26 02:35:34 +0900

    Done

---
+++ T_001_Basic/T_001_Basic/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+// crtl r g to delete all not use liblirary
+namespace T_001_Basic.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
---
+++ T_001_Basic/T_001_Basic/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+// crtl r g 
+namespace T_001_Basic
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+        // 30.22
+        public static IWebHost BuildWebHost(string[] args) {
+            return new WebHostBuilder()
+                  .UseKestrel()
+                  .UseContentRoot(Directory.GetCurrentDirectory())
+                  .UseStartup<Startup>()
+                  .Build();
+        }
+    }
+}
